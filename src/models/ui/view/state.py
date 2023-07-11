49e789bca3f4ec3f5e46e85d49119c112d23c199
Tumpa-Prizrak <92235273+Tumpa-Prizrak@users.noreply.github.com>
2023-07-11 11:49:44 +0300

    Init.

--- src/models/ui/view/state.py
+++
@@ -1,9 +0,0 @@
-from discord import ui
-from discord import Interaction
-from src.models.ui import modal
-
-
-class StateView(ui.View):
-    @ui.button(label="Check", emoji="✅")
-    async def check(self, interaction: Interaction, button):
-        await interaction.response.send_modal(modal.state.OnlineModal())  # type: ignore
