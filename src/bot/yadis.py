6d9c46f875a9038062f1ec84dd1ba37203b70c7b
Tumpa-Prizrak <igrunok.play@yanex.ru>
2023-07-18 09:36:19 +0300

    small improvments
    - Sourcery fixed
    - extension loading
    - errors are logging into different file error.py

--- src/bot/yadis.py
+++ src/bot/yadis.py
@@ -3,7 +3,8 @@ from discord import Intents
 from typing import Dict, Tuple, Any
 from logging import Logger
 from api import logger
-from api.database import api
+import os
+from api.tools import load_extensions
 
 
 class Yadis(Bot):
@@ -25,11 +26,19 @@ class Yadis(Bot):
         )
         self.token: str = token
         self.logger: Logger = logger.setup_logger()
+    
+    async def setup_hook(self) -> None:
+        for category in os.listdir("bot/cogs"):
+            obj = f"bot/cogs/{category}"
+            if os.path.isfile(obj): continue
+
+            await load_extensions(self, obj, "cog_")
+        
+        await load_extensions(self, "bot/events", "event_")
+
 
     async def on_ready(self) -> None:
         self.logger.info(f"Bot is ready! latency: {round(self.latency, 3)}")
-        for guild in self.guilds:
-            print(api.get_guild(bot=self, guild=guild).name)
 
     def run(self, token: str | None = None) -> None:  # type: ignore
         super().run(token or self.token, reconnect=True)
